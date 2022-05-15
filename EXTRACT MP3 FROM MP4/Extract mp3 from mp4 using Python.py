539ab63c70bf38c9c5763f61783e64159e1cec48
shubham <shubhamkamboj2550@gmail.com>
2022-05-15 23:50:12 +0530

    All Python Projects

---
+++ EXTRACT MP3 FROM MP4/Extract mp3 from mp4 using Python.py
@@ -0,0 +1,13 @@
+import moviepy
+import moviepy.editor
+
+
+# Get this library installed in your system
+
+# Import your video file
+# Media file should be local
+video = moviepy.editor.VideoFileClip("D:\IT\Assignments")    # Put your file path in here
+
+# Convert video to audio
+audio = video.audio
+audio.write_audiofile('new_audio.mp3')
